f0cfed0c1d4c365df1b2a45124de55624693882c
German Ermilov <german27072005@gmail.com>
2024-01-28 02:26:38 +0300

    the_snake 1.1

--- the_snake.py
+++ the_snake.py
@@ -43,9 +43,12 @@ clock = pygame.time.Clock()
 
 
 class GameObject:
-    position = ((SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2))
+    """Родительский класс, 
+    который описывает игровой объект (позиция и цвет).
+    """
 
-    def __init__(self, body_color, position):
+    def __init__(self, body_color=APPLE_COLOR,
+                 position=((SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2))):
         self.position = position
         self.body_color = body_color
 
@@ -54,16 +57,24 @@ class GameObject:
 
 
 class Apple(GameObject):
-    def __init__(self):
-        self.body_color = APPLE_COLOR
+    """Дочерний класс класса GameObject,
+    который описывает состояние "яблок" на игровом поле.
+    """
+
+    def __init__(self, body_color=APPLE_COLOR):
+        self.body_color = body_color
         self.position = self.randomize_position()
 
     def randomize_position(self):
+        """Метод, генерирующий случайное положение яблока на игровом поле."""
+
         width = randint(0, GRID_WIDTH) * GRID_SIZE
         height = randint(0, GRID_HEIGHT) * GRID_SIZE
         return (width, height)
 
     def draw(self, surface):
+        """Метод отрисовки яблока на игровом поле."""
+
         rect = pygame.Rect(
             (self.position[0], self.position[1]),
             (GRID_SIZE, GRID_SIZE)
@@ -73,6 +84,10 @@ class Apple(GameObject):
 
 
 class Snake(GameObject):
+    """Дочерний класс класса GameObject, который описывает состояние "змейки"
+    на игровом поле (позицию, цвет, направление движения, длину).
+    """
+
     def __init__(self):
         self.length = 1
         self.body_color = SNAKE_COLOR
@@ -80,20 +95,34 @@ class Snake(GameObject):
         self.last = None
         self.positions = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
         self.next_direction = None
+        self.position = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
     def update_direction(self):
+        """Метод, определяющий текущее направление движения
+        змейки по игровому полю.
+        """
+
         if self.next_direction:
             self.direction = self.next_direction
             self.next_direction = None
 
     def get_head_position(self):
+        """Метод, возвращающий координаты головы змейки."""
+
         return self.positions[0]
 
     def move(self):
+        """Метод, обновляющий позицию змейки, добавляя новую голову
+        в начало списка positions и удаляя последний элемент,
+        если длина змейки не увеличилась.
+        """
+
         self.last = self.positions[-1]
         current_head_position = self.get_head_position()
-        new_head_position = (current_head_position[0] + self.direction[0] * GRID_SIZE,
-                             current_head_position[1] + self.direction[1] * GRID_SIZE)
+        new_head_position = (current_head_position[0] +
+                             self.direction[0] * GRID_SIZE,
+                             current_head_position[1] +
+                             self.direction[1] * GRID_SIZE)
 
         if new_head_position[0] > SCREEN_WIDTH:
             new_head_position = (new_head_position[0] - SCREEN_WIDTH,
@@ -116,6 +145,10 @@ class Snake(GameObject):
                 self.positions.pop()
 
     def reset(self):
+        """Метод, который сбрасывает змейку в начальное состояние
+        после столкновения с собой.
+        """
+
         screen.fill(BOARD_BACKGROUND_COLOR)
         self.length = 1
         self.positions = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
@@ -123,6 +156,8 @@ class Snake(GameObject):
         self.next_direction = None
 
     def draw(self, surface):
+        """Метод отрисовки змейки на игровом поле."""
+
         for position in self.positions[:-1]:
             rect = (
                 pygame.Rect((position[0], position[1]), (GRID_SIZE, GRID_SIZE))
@@ -145,6 +180,8 @@ class Snake(GameObject):
 
 
 def handle_keys(game_object):
+    """Функция обработки нажатий клавиш игроком."""
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
